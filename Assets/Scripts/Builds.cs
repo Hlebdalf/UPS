959d403f9462a132faf498a465efdc9cb614f81a
Max Hunter <makcim.devyaterikov@gmail.com>
2020-11-01 23:09:29 +0300

    Initial

---
+++ Assets/Scripts/Builds.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Builds : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
