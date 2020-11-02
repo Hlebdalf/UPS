dcd02277a722117d2dbbc26115225f5d3b8a32f7
Max Hunter <makcim.devyaterikov@gmail.com>
2020-11-02 21:52:26 +0300

    Added function for adding objects to the scene

--- Assets/Scripts/Builds.cs
+++ Assets/Scripts/Builds.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Builds : MonoBehaviour {
+
+    public GameObject[] preFubs;
+    private List <GameObject> objects;
+
     void Start() {
-        
+        objects = new List <GameObject> ();
+        for (int i = 0; i < preFubs.Length; ++i) {
+            objects.Add(Instantiate(preFubs[i], new Vector3(0, 0, 0), Quaternion.identity));
+        }
     }
 
     void Update() {
