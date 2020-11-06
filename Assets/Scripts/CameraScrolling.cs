70691728406f4bb7dfed2f14015c7fdea9d0109e
@sarkolin123 <sarkolin123@gmail.com>
2020-11-06 22:31:16 +0300

    Added camera scrolling

---
+++ Assets/Scripts/CameraScrolling.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScrolling : MonoBehaviour
+{
+    public float roof;
+    public float floor;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.mouseScrollDelta.y > 0 && transform.position.y > floor)
+        {
+            transform.position += new Vector3(0, -Input.mouseScrollDelta.y % speed, Input.mouseScrollDelta.y % speed);
+        }
+        if (Input.mouseScrollDelta.y < 0 && transform.position.y < roof)
+        {
+            transform.position += new Vector3(0, -Input.mouseScrollDelta.y % speed, Input.mouseScrollDelta.y % speed);
+        }
+    }
+}
