4e8f5cc6e0514f19c1119d519808493742ed6972
@sarkolin123 <sarkolin123@gmail.com>
2020-11-22 22:51:45 +0300

    created main menu

---
+++ Assets/Scripts/InterfaceScripts/LoadPanelBlur.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LoadPanelBlur : MonoBehaviour
+{
+    public float blur = 0;
+    private Material MaterialClass;
+
+    void Start()
+    {
+        Image ImageClass = gameObject.GetComponent<Image>();
+        MaterialClass = ImageClass.material;
+    }
+
+    void FixedUpdate()
+    {
+        MaterialClass.SetFloat("_Size", blur);
+    }
+}
