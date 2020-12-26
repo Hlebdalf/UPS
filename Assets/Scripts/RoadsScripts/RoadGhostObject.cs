3ce2a3f744b167fe3bfba7189e3df0911eeb2eac
makcim.devyaterikov <makcim.devyaterikov@gmail.com>
2020-12-26 17:24:27 +0300

    Added round coordinate for roads

--- Assets/Scripts/RoadsScripts/RoadGhostObject.cs
+++ Assets/Scripts/RoadsScripts/RoadGhostObject.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class RoadGhostObject : MonoBehaviour {
     private GameObject MainCamera;
     private Roads RoadsClass;
+    private float eps = 1e-5f;
     private bool isFollow = true;
     private bool isBusy = false;
     private bool isFirst = false;
@@ -26,20 +27,22 @@ public class RoadGhostObject : MonoBehaviour {
             if (Physics.Raycast(ray, out hit)) {
                 RoadGhostObject data = gameObject.GetComponent <RoadGhostObject> ();
                 if (isFirst) {
-                    data.x1 = (int)hit.point.x;
+                    data.x1 = hit.point.x;
                     data.y1 = hit.point.z;
-                    Vector2 point = RoadsClass.RoundMovingCoordinateOnTheRoad(data, idx, connectedRoad);
-                    data.x1 = point.x;
-                    data.y1 = point.y;
+                    Vector2 point1 = RoundMovingCoordinateOnTheRoad(data, idx, connectedRoad);
+                    Vector2 point2 = RoundCoodinate(point1);
+                    data.x1 = point2.x;
+                    data.y1 = point2.y;
                 }
                 else {
-                    data.x2 = hit.point.x;
-                    data.y2 = hit.point.z;
+                    Vector2 point = RoundCoodinate(new Vector2(hit.point.x, hit.point.z));
+                    data.x2 = point.x;
+                    data.y2 = point.y;
                 }
 
                 data.len = (float)Math.Sqrt(Math.Pow(data.x2 - data.x1, 2) + Math.Pow(data.y2 - data.y1, 2));
                 transform.rotation = Quaternion.Euler(0, RoadsClass.funcAngle(len, data.x2 - data.x1, data.x1, data.y1, data.x2, data.y2), 0);
-                transform.position = new Vector3((data.x1 + data.x2) / 2, 0, (data.y1 + data.y2) / 2);
+                transform.position = new Vector3((data.x1 + data.x2) / 2, 0.2f, (data.y1 + data.y2) / 2);
                 transform.localScale = new Vector3(1, 1, data.len / 2);
 
                 MeshRenderer MeshRendererClass = gameObject.GetComponent <MeshRenderer> ();
@@ -67,7 +70,7 @@ public class RoadGhostObject : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
-                RoadObject data = RoadsClass.ghostObjects[idx].GetComponent <RoadObject> ();
+                RoadGhostObject data = RoadsClass.ghostObjects[idx].GetComponent <RoadGhostObject> ();
                 float dist1 = (float)Math.Sqrt(Math.Pow(hit.point.x - data.x1, 2) + Math.Pow(hit.point.z - data.y1, 2));
                 float dist2 = (float)Math.Sqrt(Math.Pow(hit.point.x - data.x2, 2) + Math.Pow(hit.point.z - data.y2, 2));
                 if (dist1 < dist2) isFirst = true;
@@ -76,5 +79,72 @@ public class RoadGhostObject : MonoBehaviour {
             gameObject.layer = 2;
             RoadsClass.isFollowGhost = isFollow = isBusy = true;
         }
+    }
+
+    public Vector2 RoundCoodinate(Vector2 point) {
+        int gridSize = 1;
+        float x = point.x, y = point.y, low, high;
+
+        low = (int)(x / gridSize) * gridSize;
+        high = low + gridSize;
+        if (Math.Abs(x - low) < Math.Abs(x - high)) point.x = low;
+        else point.x = high;
+        
+        low = (int)(y / gridSize) * gridSize;
+        high = low + gridSize;
+        if (Math.Abs(y - low) < Math.Abs(y - high)) point.y = low;
+        else point.y = high;
+
+        return point;
+    }
+
+    private Vector2 RoundMovingCoordinateOnTheRoad(RoadGhostObject dataGhost, int idxGhost, int idxRoad) {
+        RoadObject data = RoadsClass.objects[idxRoad].GetComponent <RoadObject> ();
+        float cursorX = dataGhost.x1;
+        float cursorY = dataGhost.y1;
+
+        float mainRoadA = data.y1 - data.y2, mainRoadB = data.x2 - data.x1, mainRoadC = data.x1 * data.y2 - data.x2 * data.y1; // main road line
+        float ghostRoadA = dataGhost.y1 - dataGhost.y2, ghostRoadB = dataGhost.x2 - dataGhost.x1, ghostRoadC = dataGhost.x1 * dataGhost.y2 - dataGhost.x2 * dataGhost.y1; // ghost road line
+        float mainRoadCrossGhostRoadX = -(mainRoadC * ghostRoadB - ghostRoadC * mainRoadB) / (mainRoadA * ghostRoadB - ghostRoadA * mainRoadB); // rounded coordinate
+        float mainRoadCrossGhostRoadY = -(mainRoadA * ghostRoadC - ghostRoadA * mainRoadC) / (mainRoadA * ghostRoadB - ghostRoadA * mainRoadB); // rounded coordinate
+
+        float normA = -mainRoadB, normB = mainRoadA, normC = -(normA * cursorX + normB * cursorY); // norm
+        float normCrossMainRoadX = -(mainRoadC * normB - normC * mainRoadB) / (mainRoadA * normB - normA * mainRoadB); // rounded coordinate
+        float normCrossMainRoadY = -(mainRoadA * normC - normA * mainRoadC) / (mainRoadA * normB - normA * mainRoadB); // rounded coordinate
+
+        float minDist = (float)Math.Sqrt(Math.Pow(normCrossMainRoadX - cursorX, 2) + Math.Pow(normCrossMainRoadY - cursorY, 2));
+        int minDistIdx = idxRoad;
+        float bestLineA = mainRoadA, bestLineB = mainRoadB, bestLineC = mainRoadC;
+        for (int i = 0; i < data.connectedRoads.Count; ++i) {
+            RoadObject tmpData = RoadsClass.objects[data.connectedRoads[i]].GetComponent <RoadObject> ();
+
+            float tmpA = tmpData.y1 - tmpData.y2, tmpB = tmpData.x2 - tmpData.x1, tmpC = tmpData.x1 * tmpData.y2 - tmpData.x2 * tmpData.y1; // tmp road line
+            float tmpNormA = -tmpB, tmpNormB = tmpA, tmpNormC = -(tmpNormA * cursorX + tmpNormB * cursorY); // norm
+            float tmpNormCrossTmpX = -(tmpC * tmpNormB - tmpNormC * tmpB) / (tmpA * tmpNormB - tmpNormA * tmpB); // rounded coordinate
+            float tmpNormCrossTmpY = -(tmpA * tmpNormC - tmpNormA * tmpC) / (tmpA * tmpNormB - tmpNormA * tmpB); // rounded coordinate
+
+            float tmpDist = (float)Math.Sqrt(Math.Pow(tmpNormCrossTmpX - cursorX, 2) + Math.Pow(tmpNormCrossTmpY - cursorY, 2));
+            if (tmpDist < minDist) {
+                minDist = tmpDist;
+                minDistIdx = data.connectedRoads[i];
+                bestLineA = tmpNormA;
+                bestLineB = tmpNormB;
+                bestLineC = tmpNormC;
+            }
+        }
+
+        float x = -(bestLineC * ghostRoadB - ghostRoadC * bestLineB) / (bestLineA * ghostRoadB - ghostRoadA * bestLineB); // rounded coordinate
+        float y = -(bestLineA * ghostRoadC - ghostRoadA * bestLineC) / (bestLineA * ghostRoadB - ghostRoadA * bestLineB); // rounded coordinate
+
+        Vector2 ans = new Vector2(x, y);
+        RoadsClass.ghostObjects[idxGhost].GetComponent <RoadGhostObject> ().connectedRoad = minDistIdx;
+        float dist1 = (float)Math.Sqrt(Math.Pow(mainRoadCrossGhostRoadX - data.x1, 2) + Math.Pow(mainRoadCrossGhostRoadY - data.y1, 2));
+        float dist2 = (float)Math.Sqrt(Math.Pow(mainRoadCrossGhostRoadX - data.x2, 2) + Math.Pow(mainRoadCrossGhostRoadY - data.y2, 2));
+        float dist = (float)Math.Sqrt(Math.Pow(data.x2 - data.x1, 2) + Math.Pow(data.y2 - data.y1, 2));
+        if (dist1 + dist2 - dist > eps) {
+            if (dist1 < dist2) ans = new Vector2(data.x1, data.y1);
+            else ans = new Vector2(data.x2, data.y2);
+        }
+        return ans;
     }
 }
--- Assets/Scripts/RoadsScripts/Roads.cs
+++ Assets/Scripts/RoadsScripts/Roads.cs
@@ -26,8 +26,7 @@ public class Roads : MonoBehaviour {
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit)) {
                 if (Input.GetMouseButtonDown(0)) {
-                    Vector2 point = RoundCoordinateOnTheRoad(new Vector2(hit.point.x, hit.point.z), idxOverRoad);
-                    CreateGhost(RoadType, new Vector3(point.x, 0, point.y), idxOverRoad);
+                    CreateGhost(RoadType, RoundCoordinateOnTheRoad(hit.point, idxOverRoad), idxOverRoad);
                 }
             }
         }
@@ -56,6 +55,42 @@ public class Roads : MonoBehaviour {
         MeshRendererClass.materials[0].SetTextureScale("_MainTex", new Vector2(len / 2, 1));
     }
 
+    private Vector3 RoundCoordinateOnTheRoad(Vector3 point, int idxRoad) {
+        RoadObject data = objects[idxRoad].GetComponent <RoadObject> ();
+        float a1 = data.y1 - data.y2, b1 = data.x2 - data.x1, c1 = data.x1 * data.y2 - data.x2 * data.y1; // line
+        float a2 = -b1, b2 = a1, c2 = -(a2 * point.x + b2 * point.z); // norm
+        if (a1 * b2 - a2 * b1 == 0) return point; // parallel
+        float x = -(c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
+        float y = -(a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
+        Vector3 ans = new Vector3(x, 0, y);
+        float dist1 = (float)Math.Sqrt(Math.Pow(x - data.x1, 2) + Math.Pow(y - data.y1, 2));
+        float dist2 = (float)Math.Sqrt(Math.Pow(x - data.x2, 2) + Math.Pow(y - data.y2, 2));
+        float dist = (float)Math.Sqrt(Math.Pow(data.x2 - data.x1, 2) + Math.Pow(data.y2 - data.y1, 2));
+        if (dist1 + dist2 - dist > eps) {
+            if (dist1 < dist2) ans = new Vector3(data.x1, 0, data.y1);
+            else ans = new Vector3(data.x2, 0, data.y2);
+        }
+        ans = RoundCoodinate(ans);
+        return ans;
+    }
+
+    private Vector3 RoundCoodinate(Vector3 point) {
+        int gridSize = 1;
+        float x = point.x, z = point.z, low, high;
+
+        low = (int)(x / gridSize) * gridSize;
+        high = low + gridSize;
+        if (Math.Abs(x - low) < Math.Abs(x - high)) point.x = low;
+        else point.x = high;
+        
+        low = (int)(z / gridSize) * gridSize;
+        high = low + gridSize;
+        if (Math.Abs(z - low) < Math.Abs(z - high)) point.z = low;
+        else point.z = high;
+
+        return point;
+    }
+
     private int ToIndex(string name) {
         for (int i = 0; i < preFubs.Length; ++i) {
             if (preFubs[i].name == name || preFubsGhost[i].name == name) {
@@ -71,74 +106,6 @@ public class Roads : MonoBehaviour {
             if (y1 <= y2) return (float)(90 - Math.Acos(leg / dist) * 57.3);
             else return (float)(-270 + Math.Acos(leg / dist) * 57.3);
         }
-    }
-
-    public Vector2 RoundCoordinateOnTheRoad(Vector2 point, int idxRoad) {
-        RoadObject data = objects[idxRoad].GetComponent <RoadObject> ();
-        float a1 = data.y1 - data.y2, b1 = data.x2 - data.x1, c1 = data.x1 * data.y2 - data.x2 * data.y1; // line
-        float a2 = -b1, b2 = a1, c2 = -(a2 * point.x + b2 * point.y); // norm
-        if (a1 * b2 - a2 * b1 == 0) return point; // parallel
-        float x = -(c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1);
-        float y = -(a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
-        Vector2 ans = new Vector2(x, y);
-        float dist1 = (float)Math.Sqrt(Math.Pow(x - data.x1, 2) + Math.Pow(y - data.y1, 2));
-        float dist2 = (float)Math.Sqrt(Math.Pow(x - data.x2, 2) + Math.Pow(y - data.y2, 2));
-        float dist = (float)Math.Sqrt(Math.Pow(data.x2 - data.x1, 2) + Math.Pow(data.y2 - data.y1, 2));
-        if (dist1 + dist2 - dist > eps) {
-            if (dist1 < dist2) ans = new Vector2(data.x1, data.y1);
-            else ans = new Vector2(data.x2, data.y2);
-        }
-        return ans;
-    }
-
-    public Vector2 RoundMovingCoordinateOnTheRoad(RoadGhostObject dataGhost, int idxGhost, int idxRoad) {
-        RoadObject data = objects[idxRoad].GetComponent <RoadObject> ();
-        float cursorX = dataGhost.x1;
-        float cursorY = dataGhost.y1;
-
-        float mainRoadA = data.y1 - data.y2, mainRoadB = data.x2 - data.x1, mainRoadC = data.x1 * data.y2 - data.x2 * data.y1; // main road line
-        float ghostRoadA = dataGhost.y1 - dataGhost.y2, ghostRoadB = dataGhost.x2 - dataGhost.x1, ghostRoadC = dataGhost.x1 * dataGhost.y2 - dataGhost.x2 * dataGhost.y1; // ghost road line
-        float mainRoadCrossGhostRoadX = -(mainRoadC * ghostRoadB - ghostRoadC * mainRoadB) / (mainRoadA * ghostRoadB - ghostRoadA * mainRoadB); // rounded coordinate
-        float mainRoadCrossGhostRoadY = -(mainRoadA * ghostRoadC - ghostRoadA * mainRoadC) / (mainRoadA * ghostRoadB - ghostRoadA * mainRoadB); // rounded coordinate
-
-        float normA = -mainRoadB, normB = mainRoadA, normC = -(normA * cursorX + normB * cursorY); // norm
-        float normCrossMainRoadX = -(mainRoadC * normB - normC * mainRoadB) / (mainRoadA * normB - normA * mainRoadB); // rounded coordinate
-        float normCrossMainRoadY = -(mainRoadA * normC - normA * mainRoadC) / (mainRoadA * normB - normA * mainRoadB); // rounded coordinate
-
-        float minDist = (float)Math.Sqrt(Math.Pow(normCrossMainRoadX - cursorX, 2) + Math.Pow(normCrossMainRoadY - cursorY, 2));
-        int minDistIdx = idxRoad;
-        float bestLineA = mainRoadA, bestLineB = mainRoadB, bestLineC = mainRoadC;
-        for (int i = 0; i < data.connectedRoads.Count; ++i) {
-            RoadObject tmpData = objects[data.connectedRoads[i]].GetComponent <RoadObject> ();
-
-            float tmpA = tmpData.y1 - tmpData.y2, tmpB = tmpData.x2 - tmpData.x1, tmpC = tmpData.x1 * tmpData.y2 - tmpData.x2 * tmpData.y1; // tmp road line
-            float tmpNormA = -tmpB, tmpNormB = tmpA, tmpNormC = -(tmpNormA * cursorX + tmpNormB * cursorY); // norm
-            float tmpNormCrossTmpX = -(tmpC * tmpNormB - tmpNormC * tmpB) / (tmpA * tmpNormB - tmpNormA * tmpB); // rounded coordinate
-            float tmpNormCrossTmpY = -(tmpA * tmpNormC - tmpNormA * tmpC) / (tmpA * tmpNormB - tmpNormA * tmpB); // rounded coordinate
-
-            float tmpDist = (float)Math.Sqrt(Math.Pow(tmpNormCrossTmpX - cursorX, 2) + Math.Pow(tmpNormCrossTmpY - cursorY, 2));
-            if (tmpDist < minDist) {
-                minDist = tmpDist;
-                minDistIdx = data.connectedRoads[i];
-                bestLineA = tmpNormA;
-                bestLineB = tmpNormB;
-                bestLineC = tmpNormC;
-            }
-        }
-
-        float x = -(bestLineC * ghostRoadB - ghostRoadC * bestLineB) / (bestLineA * ghostRoadB - ghostRoadA * bestLineB); // rounded coordinate
-        float y = -(bestLineA * ghostRoadC - ghostRoadA * bestLineC) / (bestLineA * ghostRoadB - ghostRoadA * bestLineB); // rounded coordinate
-
-        Vector2 ans = new Vector2(x, y);
-        ghostObjects[idxGhost].GetComponent <RoadGhostObject> ().connectedRoad = minDistIdx;
-        float dist1 = (float)Math.Sqrt(Math.Pow(mainRoadCrossGhostRoadX - data.x1, 2) + Math.Pow(mainRoadCrossGhostRoadY - data.y1, 2));
-        float dist2 = (float)Math.Sqrt(Math.Pow(mainRoadCrossGhostRoadX - data.x2, 2) + Math.Pow(mainRoadCrossGhostRoadY - data.y2, 2));
-        float dist = (float)Math.Sqrt(Math.Pow(data.x2 - data.x1, 2) + Math.Pow(data.y2 - data.y1, 2));
-        if (dist1 + dist2 - dist > eps) {
-            if (dist1 < dist2) ans = new Vector2(data.x1, data.y1);
-            else ans = new Vector2(data.x2, data.y2);
-        }
-        return ans;
     }
 
     public void CreateGhost(string name, Vector3 point, int idxRoad) {
@@ -161,7 +128,7 @@ public class Roads : MonoBehaviour {
         for (int i = 0; i < ghostObjects.Count; ++i) {
             GameObject ghostObject = ghostObjects[i];
             RoadGhostObject ghostObjectClass = ghostObject.GetComponent <RoadGhostObject> ();
-            objects.Add(Instantiate(preFubs[ghostObjectClass.idx], ghostObject.transform.position, ghostObject.transform.rotation));
+            objects.Add(Instantiate(preFubs[ghostObjectClass.idx], new Vector3(ghostObject.transform.position.x, 0, ghostObject.transform.position.z), ghostObject.transform.rotation));
             objects[objects.Count - 1].AddComponent <RoadObject> ();
             RoadObject objectClass = objects[objects.Count - 1].GetComponent <RoadObject> ();
 
