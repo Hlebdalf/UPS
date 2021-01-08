3c97de5db897553998829ad18927f71a8c7de511
makcim.devyaterikov <makcim.devyaterikov@gmail.com>
2021-01-08 08:02:07 +0300

    Fix connect to crossroad

--- Assets/Scripts/RoadsScripts/CrossroadObject.cs
+++ Assets/Scripts/RoadsScripts/CrossroadObject.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CrossroadObject : MonoBehaviour {
+    private GameObject MainCamera;
+    private Roads RoadsClass;
+
     public float x, y;
     public int idx, xr, yr;
     public List <int> connectedRoads;
 
     private void Awake() {
+        MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        RoadsClass = MainCamera.GetComponent <Roads> ();
         connectedRoads = new List <int> ();
+    }
+
+    private void OnMouseOver() {
+        RoadsClass.idxOverCrossroad = idx;
+    }
+
+    private void OnMouseExit() {
+        RoadsClass.idxOverCrossroad = -1;
     }
 }
