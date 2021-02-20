4b48a07ef7bd1cd48e41d72f4e158841db526f16
makcim.devyaterikov <makcim.devyaterikov@gmail.com>
2021-02-20 16:43:09 +0300

    Create upgrades script

---
+++ Assets/Scripts/GameScripts/Upgrades.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour {
+    private Economy EconomyClass;
+    private Builds BuildsClass;
+    private People PeopleClass;
+
+    private void Awake() {
+        EconomyClass = Camera.main.GetComponent <Economy> ();
+        BuildsClass = Camera.main.GetComponent <Builds> ();
+        PeopleClass = Camera.main.GetComponent <People> ();
+    }
+}
