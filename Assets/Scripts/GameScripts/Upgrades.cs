d90faf3fec917441bbf68cfe16caaaf8e701b244
makcim.devyaterikov <makcim.devyaterikov@gmail.com>
2021-02-21 18:00:02 +0300

    Create functions for upgrades

--- Assets/Scripts/GameScripts/Upgrades.cs
+++ Assets/Scripts/GameScripts/Upgrades.cs
@@ -13,6 +13,20 @@ public class Upgrades : MonoBehaviour {
         PeopleClass = Camera.main.GetComponent <People> ();
     }
 
+    // Глобальные улучшения города
+    public void TheReformOfTheRoad() {
+        for (int i = 0; i < BuildsClass.commerces.Count; ++i) {
+            if (BuildsClass.commerces[i].GetComponent <Factory> ()) {
+                Factory factoryClass = BuildsClass.commerces[i].GetComponent <Factory> ();
+                // factoryClass.ChangeProductsRate(x);
+            }
+            else if (BuildsClass.commerces[i].GetComponent <Science> ()) {
+                Science scienceClass = BuildsClass.commerces[i].GetComponent <Science> ();
+                // scienceClass.ChangeScienceRate(x);
+            }
+        }
+    }
+
     public void ImprovingTheQualityOfHolograms() {
         for (int i = 0; i < PeopleClass.objects.Count; ++i) {
             Passport passportClass = PeopleClass.objects[i].GetComponent <Passport> ();
@@ -46,6 +60,33 @@ public class Upgrades : MonoBehaviour {
             }
         }
     }
+    
+    public void Science_PromotionOfScience() {
+        for (int i = 0; i < BuildsClass.commerces.Count; ++i) {
+            if (BuildsClass.commerces[i].GetComponent <Science> ()) {
+                Science scienceClass = BuildsClass.commerces[i].GetComponent <Science> ();
+                // scienceClass.ChangeScienceRate(x);
+            }
+        }
+    }
+
+    public void Science_ForeignSpecialists() {
+        for (int i = 0; i < BuildsClass.commerces.Count; ++i) {
+            if (BuildsClass.commerces[i].GetComponent <Science> ()) {
+                Science scienceClass = BuildsClass.commerces[i].GetComponent <Science> ();
+                // scienceClass.ChangeScienceRate(x);
+            }
+        }
+    }
+
+    public void Science_StudentExchangePrograms() {
+        for (int i = 0; i < BuildsClass.commerces.Count; ++i) {
+            if (BuildsClass.commerces[i].GetComponent <Science> ()) {
+                Science scienceClass = BuildsClass.commerces[i].GetComponent <Science> ();
+                // scienceClass.ChangeScienceRate(x);
+            }
+        }
+    }
 
     // Глобальные улучшения заводов
     public void Factory_NewProductionMethods() {
@@ -56,6 +97,8 @@ public class Upgrades : MonoBehaviour {
             }
         }
     }
+
+    public void Factory_NewPartners() {}
 
     public void Factory_NewSuppliers() {
         for (int i = 0; i < BuildsClass.commerces.Count; ++i) {
@@ -75,4 +118,25 @@ public class Upgrades : MonoBehaviour {
             }
         }
     }
+
+    // Глобальные улучшения домов
+    public void House_CentralHeatingSystem() {
+        for (int i = 0; i < PeopleClass.objects.Count; ++i) {
+            Passport passportClass = PeopleClass.objects[i].GetComponent <Passport> ();
+            // passportClass.satisfaction += k;
+        }
+    }
+
+    // Глобальные улучшения людей
+    public void People_SimplificationOfBorderControl() {}
+
+    public void People_AdvertisingOfTheCity() {}
+
+    public void People_BenefitsForLargeFamilies() {}
+
+    public void People_PersonalIncomeTaxReduction() {}
+
+    public void People_PromotionOfLargeFamilies() {}
+
+    public void People_PreferentialHousingServices() {}
 }
