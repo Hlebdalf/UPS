796d2d3fe6d04543df70994a5f2502320aec2879
makcim.devyaterikov <makcim.devyaterikov@gmail.com>
2021-05-08 19:48:15 +0300

    Fix cars

--- Assets/Scripts/GameScripts/CarsScripts/Car.cs
+++ Assets/Scripts/GameScripts/CarsScripts/Car.cs
@@ -13,6 +13,7 @@ public class Car : MonoBehaviour {
     public float acceleration = 0.5f, braking = 0.5f;
     public int mainNumOfLane = 2, numOfLane = 2, idxRoad = -1;
     public bool onVisibleInCamera = false, isFollowTheFront = false, isStop = false, inCrossroad = false, canChangeLane = false;
+    public GameObject sideCarLeft, sideCarRight;
 
     private void Awake() {
         MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
@@ -24,38 +25,44 @@ public class Car : MonoBehaviour {
     private void Update() {
         if (onVisibleInCamera) {
             isFollowTheFront = isStop = false;
-            RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, Distance);
-            for (int i = 0; i < hits.Length; ++i) {
-                RaycastHit hit = hits[i];
-                if (hit.collider.gameObject.tag == "Car") {
-                    Car carClass = hit.collider.gameObject.GetComponent <Car> ();
-                    if (carClass.idxRoad == idxRoad || (carClass.inCrossroad && inCrossroad)) {
-                        isFollowTheFront = true;
-                        speedTheFront = carClass.speed;
+            {
+                RaycastHit[] hits = Physics.RaycastAll(transform.position, transform.forward, Distance);
+                for (int i = 0; i < hits.Length; ++i) {
+                    RaycastHit hit = hits[i];
+                    if (hit.collider.gameObject.tag == "Car") {
+                        Car carClass = hit.collider.gameObject.GetComponent <Car> ();
+                        if (carClass.idxRoad == idxRoad || (carClass.inCrossroad && inCrossroad)) {
+                            isFollowTheFront = true;
+                            speedTheFront = carClass.speed;
+                        }
+                    }
+                    else if (hit.collider.gameObject.tag == "TrafficLight" && !inCrossroad) {
+                        CrossroadObject crossroadObjectClass = hit.collider.gameObject.GetComponent <CrossroadObject> ();
+                        if (crossroadObjectClass.idxRoadGO == idxRoad || idxRoad < 0 || crossroadObjectClass.idxRoadGO < 0) isStop = false;
+                        else isStop = true;
                     }
                 }
-                else if (hit.collider.gameObject.tag == "TrafficLight" && !inCrossroad) {
-                    CrossroadObject crossroadObjectClass = hit.collider.gameObject.GetComponent <CrossroadObject> ();
-                    if (crossroadObjectClass.idxRoadGO == idxRoad || idxRoad < 0 || crossroadObjectClass.idxRoadGO < 0) isStop = false;
-                    else isStop = true;
-                }
+                if (isStop) speed = Math.Max(speed - braking, 0f);
+                else if (isFollowTheFront) speed = Math.Max(Math.Max(speedTheFront - 1f, speed - braking), 0f);
+                else speed = Math.Min(mainSpeed, speed + acceleration);
             }
-            if (isStop) speed = Math.Max(speed - braking, 0f);
-            else if (isFollowTheFront) speed = Math.Max(Math.Max(speedTheFront - 1f, speed - braking), 0f);
-            else speed = Math.Min(mainSpeed, speed + acceleration);
 
             if (mainNumOfLane != numOfLane) {
                 bool p = true;
                 Vector3 dir = transform.forward;
                 if (numOfLane > mainNumOfLane)  dir = transform.right;
-                else dir = transform.right;
-                Ray ray = new Ray(transform.position, dir);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit, 0.5f)) {
-                    if (hit.collider.gameObject.name == "SideCar") p = false;
+                else dir = -transform.right;
+                RaycastHit[] hits = Physics.RaycastAll(transform.position, dir, 0.25f);
+                for (int i = 0; i < hits.Length; ++i) {
+                    RaycastHit hit = hits[i];
+                    if (hit.collider.gameObject.name == "SideCar" && hit.collider.gameObject != sideCarLeft && hit.collider.gameObject != sideCarRight) {
+                        p = false;
+                        break;
+                    }
                 }
+                
                 canChangeLane = p;
-                if (!p) speed = Math.Max(speed - 0.5f, 0f);
+                if (!p) speed = Math.Max(speed - braking, 0f);
             }
         }
         else speed = mainSpeed;
